078c28980d24ecccc3356193964d028105dd1e21
pinienbaum <rafaelpinho@gmail.com>
2015-09-06 14:47:07 +0000

    Create 6of6.js

---
+++ javascript/building-an-address-book/6of6.js
@@ -0,0 +1,51 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+	var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+		printPerson(contacts[i]);
+	}
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+var search = function(lastName) {
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+        if (contacts[i].lastName === lastName) {
+            printPerson(contacts[i]);
+        }
+    }
+}
+
+var add = function(firstName, lastName, email, phoneNumber) {
+    object = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phoneNumber: phoneNumber
+    }
+    contacts[contacts.length] = object;
+
+}
+
+add("Jay", "Barman", "jb@example.com", "987-6543");
+list(contacts);
