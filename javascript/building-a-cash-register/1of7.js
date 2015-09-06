4d1f6956c907239067196b5684eaa8680baa1c03
pinienbaum <rafaelpinho@gmail.com>
2015-09-06 14:46:00 +0000

    Create 1of7.js

---
+++ javascript/building-a-cash-register/1of7.js
@@ -0,0 +1,9 @@
+//Create the object called cashRegister 
+//and initialize its total property
+var cashRegister = {
+    total: 0
+};
+
+cashRegister.total = 2.99;
+
+//Using dot notation change the total property
